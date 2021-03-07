2728749870759af7a48a4a23e887dbed5b0a6523
Nathan Moritz <nateim3@gmail.com>
2021-03-07 02:06:48 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"Gin-Rest-API/Models"
+	"Gin-Rest-API/Config"
+	"Gin-Rest-API/Routes"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status: ", err)
+	}
+
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+
+	r := Routes.SetupRouter()
+	// running
+	r.Run()
+}
